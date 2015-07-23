f9f2f8dda1f02a9a2484f8ce028754335ff894ef
Samuel Lampa <samuel.lampa@gmail.com>
2015-07-23 19:36:36 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+mkdir -p ~/opt/
+mkdir -p ~/code/go/{src,pkg,bin}
+
+cd ~/opt
+gotarball=go1.4.2.linux-amd64.tar.gz
+if [[ ! -f $gotarball ]]; then
+	wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+	tar -zxvf $gotarball
+else
+	echo "tarball already exists, so not downloading"
+fi;
+
+GOROOT=~/opt/go
+GOPATH=~/code/go
+
+echo '' >> ~/.bashrc_local
+echo '# Golang stuff' >> ~/.bashrc_local
+echo 'export GOROOT='$GOROOT >> ~/.bashrc_local
+echo 'export GOPATH='$GOPATH >> ~/.bashrc_local
+echo 'export PATH='$GOROOT'/bin:$PATH' >> ~/.bashrc_local
+echo 'export PATH='$GOPATH'/bin:$PATH' >> ~/.bashrc_local
+
+echo '. .bashrc_local' >> ~/.bashrc
+
+source ~/.bashrc
+
+go get github.com/nsf/gocode
+go install github.com/nsf/gocode
+
+echo "Now, open vim, and get Go auto-completion using <C-x><C-o>!"
